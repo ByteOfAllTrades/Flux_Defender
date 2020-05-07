48280043fab7d1cea7e184feff51ef78e31f68bd
KoboldCoder <62194534+ByteOfAllTrades@users.noreply.github.com>
2020-05-06 22:11:07 -0600

    Almost done with the HUD
    Been working on a graphically pleasing HUD for the game, I think I've got it :)

---
+++ Flux Knight/Assets/Game-Scripts/Hud_Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hud_Controller : MonoBehaviour
+{
+    public Slider healthBar;
+    public Slider energyBar;
+    public Text coinTotal;
+    public Text popMsg;
+    public Text levelTitle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar = GameObject.Find("HealthBar").GetComponent<Slider>();
+        energyBar =  GameObject.Find("EnergyBar").GetComponent<Slider>();
+        coinTotal = GameObject.Find("coinTotal").GetComponent<Text>();
+        popMsg = GameObject.Find("popMsg").GetComponent<Text>();
+        levelTitle = GameObject.Find("levelTitle").GetComponent<Text>();
+        PlayerPrefs.SetFloat("currentEnergy", 20);
+        PlayerPrefs.SetFloat("currentHealth", 20);
+        PlayerPrefs.SetString("popMsg", "Test test");
+        PlayerPrefs.SetInt("coinTotal", 100);
+        PlayerPrefs.SetString("levelTitle", "Default Dungeon");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.value = (1 / PlayerPrefs.GetFloat("maxHealth")) * PlayerPrefs.GetFloat("currentHealth");
+        energyBar.value = 0.01f * PlayerPrefs.GetFloat("currentEnergy");
+        coinTotal.text = PlayerPrefs.GetInt("coinTotal").ToString();
+        popMsg.text = PlayerPrefs.GetString("popMsg");
+        levelTitle.text = PlayerPrefs.GetString("levelTitle");
+    }
+}
